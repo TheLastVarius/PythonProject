037bcca75c3c2aafcd15d421ccd916ae934d1172
Ganjurbas <ganjurbas@gmail.com>
2015-06-22 08:37:25 +0300

    Last Update

--- test.py
+++ test.py
@@ -2,8 +2,8 @@
 import Tkinter
 
 root=Tkinter.Tk()
-root.geometry("300x200")
-
+root.geometry("400x300")
+root.title("Сборник статей")
 def thing_listbox_bind(*args, **kwargs):
     print (thing_listbox.curselection())
 
@@ -11,8 +11,12 @@ thing_listbox = Tkinter.Listbox(root, selectmode= Tkinter.SINGLE)
 thing_list_file = open ("thing_list", "r")
 for element in thing_list_file:
     thing_listbox.insert(Tkinter.END, element)
-thing_listbox.pack()
+thing_listbox.grid(row = 1, column = 1)
 thing_list_file.close()
+
+
+thing_article_list = Tkinter.Listbox(root, selectmode = Tkinter.SINGLE)
+thing_article_list.grid(row = 1, column = 3)
 
 thing_listbox.bind("<ButtonRelease-1>", thing_listbox_bind)
 
